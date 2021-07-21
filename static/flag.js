ba08b3b856f0af4a0eda2e0b7b134089f819bfef
64andy <64andy2000@gmail.com>
2021-07-21 19:47:34 +1200

    Now pixel reading isn't working

--- static/flag.js
+++ static/flag.js
@@ -1,10 +1,10 @@
-const cnv = document.getElementById("yes");
+const mainCanvas = document.getElementById("yes");
 /** @type {CanvasRenderingContext2D} */
-const ctx = cnv.getContext("2d");
+const mainCtx = mainCanvas.getContext("2d");
 
 
-const width = cnv.getAttribute("width");
-const height = cnv.getAttribute("height");
+const width = mainCanvas.getAttribute("width");
+const height = mainCanvas.getAttribute("height");
 var radius = 0;
 var isCropped = true;
 
@@ -17,8 +17,7 @@ flag.height = height;
 /** @type {CanvasRenderingContext2D} */
 const flagCtx = flag.getContext('2d');
 flagCtx.imageSmoothingEnabled = false;  // GL_LINEAR
-flagCtx.font = "30px Arial";
-flagCtx.fillText("Cum", 10, 50);
+
 
 /** @type {HTMLCanvasElement} */
 const pfp = document.createElement('canvas');
@@ -31,7 +30,7 @@ const pfpCtx = pfp.getContext('2d');
 function init() {
     console.log("init()");
     // Save the initial context state because clips can't be removed otherwise
-    ctx.save();
+    mainCtx.save();
     // Reset the form because my browser doesn't on F5
     document.querySelector('form').reset();
 
@@ -40,20 +39,34 @@ function init() {
     flagUpload.addEventListener('change', (e) => {
         readImage(e.target.files[0])
         .then((flagImg) => {
-            flagCtx.drawImage(flagImg, 0, 0, width, height);
-
-            renderPfp();
+            return stretchFlagForCircles(flagImg);
         })
         .catch((err) => {
-            console.error("Error in flagUpload's listener", err);
+            console.error("Error reading the flag image:", err);
+        })
+        .then((newFlagImg) => {
+            flagCtx.drawImage(newFlagImg, 0, 0, width, height);
+            debugger;
+            renderPfp(mainCanvas);
+        })
+    });
 
+    let pfpUpload = document.getElementById('pfp');
+    pfpUpload.addEventListener('change', (e) => {
+        readImage(e.target.files[0])
+        .then((pfpImg) => {
+            pfpCtx.drawImage(pfpImg, 0, 0, width, height);
+            renderPfp(mainCanvas);
+        })
+        .catch((err) => {
+            console.error("Error reading the profile pic:", err);
         });
     });
 
     let radiusChange = document.getElementById('radius');
     radiusChange.addEventListener("change", (e) => {
         updateRadius(e.target.value);
-        renderPfp();
+        renderPfp(mainCanvas);
     });
     updateRadius(radiusChange.value);
 
@@ -61,10 +74,8 @@ function init() {
     cropCheckbox.addEventListener("change", (e) => {
         let shouldCrop = e.target.checked;
         isCropped = shouldCrop;
-        renderPfp();
+        renderPfp(mainCanvas);
     })
-    
-    // renderPfp();
 }
 /**
  * Updates the radius variable & radius display
@@ -76,7 +87,8 @@ function updateRadius(rad) {
 }
 
 /** https://stackoverflow.com/a/46568146
- * @param {File} file
+ * Reads a file (from a form etc.), and returns it as an &lt; img >
+ * @param {File} file The image file
  * @returns {Promise<HTMLImageElement>} 
  */
 function readImage(file) {
@@ -103,28 +115,116 @@ function readImage(file) {
       });
 }
 
-function renderPfp() {
+/**
+ * 
+ * @param {CanvasRenderingContext2D} canvas 
+ */
+function clearCanvas(canvas) {
+    /** @type {CanvasRenderingContext2D} */
+    let ctx = canvas.getContext('2d');
     ctx.restore();
-    ctx.clearRect(0, 0, width, height);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    // OK restore doesn't work so uhhh HACK TIME
+    // Hey if anyone knows how to delete the clip then tell me
+    canvas.width--; canvas.width++;
+}
+
+/**
+ * Returns an image's pixels as a byte array.
+ * It's just the canvas.getImageData function but automated
+ * @param {HTMLImageElement} img
+ * @returns {ImageData}
+ */
+function getImagePixels(img, sx, sy, sw, sh) {
+    // This is a hack, javascript is a hack, i hate it here, help
+    let canvas = document.createElement('canvas');
+    let ctx = canvas.getContext('2d');
+    canvas.width = width;
+    canvas.height = height;
+    ctx.drawImage(img, 0, 0, width, height);
+
+    let data = ctx.getImageData(sx, sy, sw, sh);
+    console.log(data);
+    return data;
+}
+
+/**
+ * Stretches an image the closer it is to the edge, this makes it look nicer in a cropped circle
+ * @param {HTMLImageElement} img 
+ * @returns {HTMLCanvasElement}
+ */
+async function stretchFlagForCircles(img) {
+    console.log("STRETCH STARTEEEEEEDDDDD");
+    let w = width;
+    let h = height;
+    const getPos = (s, t) => {
+        // Convert back to integer coords
+        let i = Math.floor(s * (w-1));
+        let j = Math.floor(t * (h-1));
+        return (i*w + j) * 4;
+    }
+    const getPixel = (src, s, t) => {
+        let pos = getPos(s, t);
+        let px = src.slice(pos, pos+4);
+        return px;
+    }
+    const writePixel = (src, px, i, j) => {
+        let pos = (i*w + j) * 4;
+        for (let i = 0; i < px.length; i++) {
+            src[pos+i] = px[i];
+        }
+    }
+    
+    let oldPixels = getImagePixels(img, 0, 0, w, h).data;
+    let newPixels = new Uint8ClampedArray(oldPixels.length);
+    let s, t;   // x,y texture coords between [0, 1]
+    for (let i = 0; i < w; i++) {
+        s = i/(w-1);    // See? I remember my graphics class
+        //! s = Math.sin(s*Math.PI);
+        for (let j = 0; j < h; j++) { 
+            t = j/(h-1);
+            //! t = Math.sin(t*Math.PI);
+            // console.log(`${i}, ${j} = ${s}, ${t} -> ${(i*w + j)*4}`);
+            let pixel = getPixel(oldPixels, s, t);
+            writePixel(newPixels, pixel, s, t);
+        }
+    }
+    let canvas = document.createElement('canvas');
+    canvas.width = w; canvas.height = h;
+    let ctx = canvas.getContext('2d');
+    let imgData = new ImageData(newPixels, w, h);
+    ctx.putImageData(imgData, 0, 0);
+    console.log("... stretch done");
+    return canvas;
+}
+
+function renderPfp(canvas) {
+    clearCanvas(canvas);
+
+    /** @type {CanvasRenderingContext2D} */
+    let ctx = canvas.getContext('2d');
+    ctx.font = "italic small-caps 30px Comic Sans MS";
+    ctx.fillText("Sup\n>:)", 150, 200);
+    ctx.fillText("Add something", 90, 250);
+
     // Init: An overall circular clip, kinda represents what it looks like
     console.log("isCropped:", isCropped);
     if (isCropped) {
         console.log("Setting clip")
-        ctx.beginPath();
-        ctx.arc(width/2, height/2, Math.min(width, height)/2, 0, 2*Math.PI);
-        ctx.clip();
-        ctx.closePath
+        let flagCropPath = new Path2D();
+        flagCropPath.arc(width/2, height/2, height/2, 0, 2*Math.PI);
+        ctx.clip(flagCropPath);
     } else console.log("Not setting clip");
     // Step 1: Draw the flag
     ctx.drawImage(flag, 0, 0, width, height);
     
-    // Step 2: Image clip
-    // ctx.beginPath();
-    // ctx.arc(width/2, height/2, (Math.min(width, height)/2)-radius, 0, 2*Math.PI);
-    // ctx.clip();
-    // ctx.closePath();
-    // Step 3: Draw the PFP
-    // ctx.drawImage(pfp, 0, 0, width, height);
+    // Step 2a: Image clip
+    let pfpRadiusClip = new Path2D();
+    pfpRadiusClip.arc(width/2, height/2, (height/2)-radius, 0, 2*Math.PI);
+    ctx.clip(pfpRadiusClip);
+    // Step 2b: Draw the PFP
+    ctx.drawImage(pfp, 0, 0, width, height);
 }
 
 
@@ -132,7 +232,7 @@ function renderPfp() {
 
 
 function imSane() {
-    ctx.fillStyle = "#ff0";
-    ctx.fillRect(0, 0, 250, 250);
-    ctx.closePath();
+    mainCtx.fillStyle = "#ff0";
+    mainCtx.fillRect(0, 0, 250, 250);
+    mainCtx.closePath();
 }
