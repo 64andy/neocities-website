52f16730219cdd46bc8fbc0452edfa142f9df023
64andy <64andy2000@gmail.com>
2021-07-23 10:38:35 +1200

    Radius shrinks image + event refactor

--- static/flag.js
+++ static/flag.js
@@ -27,17 +27,51 @@ pfp.height = height;
 /** @type {CanvasRenderingContext2D} */
 const pfpCtx = pfp.getContext('2d');
 
-function init() {
-    console.log("init()");
-    // Save the initial context state because clips can't be removed otherwise
-    mainCtx.save();
-    // Reset the form because my browser doesn't on F5
-    document.querySelector('form').reset();
+// The elements i'd need to access
+var elems = {
+    flagUpload: document.getElementById('flag'),
+    pfpUpload: document.getElementById('pfp'),
+    radiusChange: document.getElementById('radius'),
+    cropCheckbox: document.getElementById('cropped'),
+    radiusDisplay: document.getElementById('radius-display'),
+}
 
-    // Event handlers //
-    let flagUpload = document.getElementById('flag');
-    flagUpload.addEventListener('change', (e) => {
-        readImage(e.target.files[0])
+// Method namespace: Contains events bound to the form
+var formEvents = {
+    // Methods
+    /**
+     * Reads the uploaded file (hopefully an image)
+     * and applies it to the profile pic canvas 
+     */
+    applyPfp() {
+        let file = elems.pfpUpload.files[0];
+        if (file === undefined) {
+            console.error("applyPfp: No pic uploaded")
+            return;
+        }
+        let targetCtx = pfpCtx;
+        readImage(file)
+        .then((pfpImg) => {
+            targetCtx.drawImage(pfpImg, 0, 0, width, height);
+            renderPfp(mainCanvas);
+        })
+        .catch((err) => {
+            console.error("Error reading the profile pic:", err);
+        });
+    },
+    /**
+     * Reads the file, parses it as an image, then does the flag stretch,
+     * and applies it to the flag canvas
+     */
+    applyFlag() {
+        let file = elems.flagUpload.files[0];
+        if (file === undefined) {
+            console.error("applyFlag: No pic uploaded")
+            return;
+        }
+        let targetCtx = flagCtx;
+
+        readImage(file)
         .then((flagImg) => {
             return stretchFlagForCircles(flagImg);
         })
@@ -45,44 +79,41 @@ function init() {
             console.error("Error reading the flag image:", err);
         })
         .then((newFlagImg) => {
-            flagCtx.drawImage(newFlagImg, 0, 0, width, height);
+            targetCtx.drawImage(newFlagImg, 0, 0, width, height);
             renderPfp(mainCanvas);
-        })
-    });
-
-    let pfpUpload = document.getElementById('pfp');
-    pfpUpload.addEventListener('change', (e) => {
-        readImage(e.target.files[0])
-        .then((pfpImg) => {
-            pfpCtx.drawImage(pfpImg, 0, 0, width, height);
-            renderPfp(mainCanvas);
-        })
-        .catch((err) => {
-            console.error("Error reading the profile pic:", err);
         });
-    });
+    },
 
-    let radiusChange = document.getElementById('radius');
-    radiusChange.addEventListener("change", (e) => {
-        updateRadius(e.target.value);
+    changeRadius() {
+        let rad = elems.radiusChange.value;
+        updateRadius(rad);
         renderPfp(mainCanvas);
-    });
-    updateRadius(radiusChange.value);
-
-    let cropCheckbox = document.getElementById('cropped');
-    cropCheckbox.addEventListener("change", (e) => {
-        let shouldCrop = e.target.checked;
-        isCropped = shouldCrop;
+    },
+    updateFlagCrop() {
+        isCropped = elems.cropCheckbox.checked;
         renderPfp(mainCanvas);
-    })
+    },
 }
+
+
+
+function init() {
+    // Save the initial context state because clips can't be removed otherwise
+    mainCtx.save();
+    // Trigger the events on the forms because browsers remember
+    for (const name in formEvents) {
+        formEvents[name]?.call()
+    }
+}
+
+
 /**
  * Updates the radius variable & radius display
  * @param {number} rad 
  */
 function updateRadius(rad) {
     radius = rad;
-    document.getElementById('radius-display').innerText = rad;
+    elems.radiusDisplay.innerText = rad;
 }
 
 /** https://stackoverflow.com/a/46568146
@@ -145,7 +176,6 @@ function getImagePixels(img, sx, sy, sw, sh) {
     ctx.drawImage(img, 0, 0, width, height);
 
     let data = ctx.getImageData(sx, sy, sw, sh);
-    console.log(data);
     return data;
 }
 
@@ -155,7 +185,6 @@ function getImagePixels(img, sx, sy, sw, sh) {
  * @returns {HTMLCanvasElement}
  */
 async function stretchFlagForCircles(img) {
-    console.log("STRETCH STARTEEEEEEDDDDD");
     let w = width;
     let h = height;
     const getPos = (s, t) => {
@@ -175,18 +204,20 @@ async function stretchFlagForCircles(img) {
             src[pos+i] = px[i];
         }
     }
-    
+    const calcOffset = (x) => { // See `./desmos_graph.png`
+        return 0.5+(Math.asin(2*x-1))/Math.PI;
+    }
     let oldPixels = getImagePixels(img, 0, 0, w, h).data;
     let newPixels = new Uint8ClampedArray(oldPixels.length);
     let s, t;   // x,y texture coords between [0, 1]
-    let sx, tx; // Offset by sin
+    let sx, tx; // Offset coords
     for (let i = 0; i < w; i++) {
         s = i/(w-1);    // See? I remember my graphics class
-        sx = 0.5+(Math.asin(2*s-1))/Math.PI;
+        let sx = calcOffset(s); // Semi-circle shape
+        
         for (let j = 0; j < h; j++) { 
             t = j/(h-1);
-            tx = 0.5+(Math.asin(2*t-1))/Math.PI;
-            // console.log(`${i}, ${j} = ${s}, ${t} -> ${(i*w + j)*4}`);
+            tx = calcOffset(t);
             let pixel = getPixel(oldPixels, sx, tx);
             writePixel(newPixels, pixel, s, t);
         }
@@ -196,7 +227,6 @@ async function stretchFlagForCircles(img) {
     let ctx = canvas.getContext('2d');
     let imgData = new ImageData(newPixels, w, h);
     ctx.putImageData(imgData, 0, 0);
-    console.log("... stretch done");
     return canvas;
 }
 
@@ -210,7 +240,6 @@ function renderPfp(canvas) {
     ctx.fillText("Add something", 90, 250);
 
     // Init: An overall circular clip, kinda represents what it looks like
-    console.log("isCropped:", isCropped);
     if (isCropped) {
         let flagCropPath = new Path2D();
         flagCropPath.arc(width/2, height/2, height/2, 0, 2*Math.PI);
@@ -224,9 +253,7 @@ function renderPfp(canvas) {
     pfpRadiusClip.arc(width/2, height/2, (height/2)-radius, 0, 2*Math.PI);
     ctx.clip(pfpRadiusClip);
     // Step 2b: Draw the PFP
-    // let pythag = radius * Math.SQRT1_2
-    // TODO: Figure out the mathematics of not making this look shit
-    ctx.drawImage(pfp, radius, radius, width-radius/2, height-radius/2);
+    ctx.drawImage(pfp, radius, radius, width-radius*2, height-radius*2);
 }
 
 
