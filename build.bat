9d8db319aaf5eb290311b5175a8da3c5e5ac9dab
64andy <64andy2000@gmail.com>
2021-08-22 13:53:55 +1200

    Minor tweaks

---
+++ build.bat
@@ -0,0 +1,6 @@
+@REM This is a dance because 'permission denied' when Jekyll
+@REM builds the site and tries to copy `img` to `_site`
+xcopy img _site\img /E /V /Y
+rename img _img
+bundle exec jekyll serve
+rename _img img
