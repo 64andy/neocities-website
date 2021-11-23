b0651cba7b80e82ae78c141926158c4f680843ce
64andy <64andy2000@gmail.com>
2021-11-23 16:10:09 +1300

    I am very dumb and didn't remember to rename _img

--- build.bat
+++ build.bat
@@ -1,5 +1,8 @@
 @REM This is a dance because 'permission denied' when Jekyll
 @REM builds the site and tries to copy `img` to `_site`
+if exist _img (
+    move _img img
+)
 xcopy img _site\img /E /V /Y
 rename img _img
 bundle exec jekyll serve
