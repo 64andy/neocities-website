548d2758377458c5f4b4db22856d8a408432449d
64andy <64andy2000@gmail.com>
2024-11-26 15:05:06 +1300

    Issue: The TS compilation comes before _site is copied over, so they get deleted instantly

--- _plugins/compile_to_ts.rb
+++ _plugins/compile_to_ts.rb
@@ -6,8 +6,8 @@
 # (If this looks ameteur it's because it is)
 # (I got here by poking at the static_file.rb source code)
 
-module TS_to_JS
-  class Generator < Jekyll::Generator
+module CompileTS
+  class RunCompiler < Jekyll::Generator
     # This method is auto-run at build-time
     def generate(site)
       # First, check if we have TypeScript installed
@@ -16,43 +16,11 @@ module TS_to_JS
           "Missing dependency",
           "`tsc` is not installed on this machine, .ts files won't be compiled"
         )
-        return
+      else
+        Jekyll.logger.info("Compiling TypeScript")
+        system("echo The current working directory is: %cd%")
+        system("tsc --build --verbose")
       end
-      # Replace every TypeScript file with our new class
-      site.static_files = site.static_files.map {
-        |file|
-          File.extname(file.path).downcase == ".ts" ?
-            TSFile.new(site, file) :
-            file
-      }
     end
   end
 end
-
-# Jekyll's static files do a fairly simple check:
-# - if the file hasn't been modified, stop
-# - otherwise copy into _site (or whatever output folder)
-#
-# We don't want to copy, we want to compile into TypeScript
-
-class TSFile < Jekyll::StaticFile
-  def initialize(site, file)
-    super(site, site.source, file.instance_variable_get("@dir"), file.name)
-  end
-
-  # @Override
-  def copy_file(dest_path)
-    # Change the extension from .ts to .js
-    dest_path = dest_path[0..-4] + ".js"
-
-    # Run the TypeScript compiler in the shell.
-    print "  compiling #{File.join(@dir, @name)} to #{dest_path}..."
-    system("tsc",
-      "--outFile", dest_path,
-      @path
-    )
-    puts "done"
-
-  end
-end
-
