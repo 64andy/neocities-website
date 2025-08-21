82567b031baad057cd3f55a70452241cf920e988
64andy <64andy2000@gmail.com>
2025-08-21 16:44:42 +1200

    The "Last modified" at the bottom of each page now shows when it was last committed in Git, instead of when the file was modified (improves portability)

--- _plugins/last_modified.rb
+++ _plugins/last_modified.rb
@@ -3,7 +3,9 @@
 module Jekyll
   module MyFilters
     def file_date(input)
-      File.mtime(input)
+      # We're showing when it was last committed to Git, instead of last modified on disk.
+      # This means cloning the repo on different machine shouldn't change the displayed date 
+      return Time.at(`git log -1 --format="%ct" -- #{input}`.to_i)
     end
   end
 end
